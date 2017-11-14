46378fbf9a329b8cd29d78a5ee14b8eb2b465dbd
Pepe Beauregard <0jmbea0@Pepes-MacBook-Pro.local>
2017-11-13 22:31:03 -0600

    Project routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
 import {FormsModule} from "@angular/forms";
+import { routing } from './app.routing';
 
 import { MessageComponent } from "./message/message.component";
+import { LoginComponent } from "./login/login.component";
+
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent
+        MessageComponent,
+        LoginComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- assets/app/login/login.component.ts
+++ assets/app/login/login.component.ts
@@ -2,8 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'login',
-    templateUrl: './message.component.html',
-
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 
 })
 export class LoginComponent{
