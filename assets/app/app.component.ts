dae2a878e0e3cce65b484eb5f56abd15149a3634
Pepe Beauregard <0jmbea0@Pepes-MacBook-Pro.local>
2017-11-13 21:21:17 -0600

    Project Structure

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -4,7 +4,7 @@ import {Message} from "./message/message.model";
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
-
+    
 })
 export class AppComponent {
     message: Message = new Message('Message Content','Author Message');
