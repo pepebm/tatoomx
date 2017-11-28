6014fe4915d451fe913f3539f93c64a0d3ffcb23
Victor Rendon <victor.rs@eucledian.mx>
2017-11-27 23:03:01 -0600

    new files

---
+++ assets/app/artist/artist.component.ts
@@ -0,0 +1,8 @@
+import {Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'artist',
+  templateUrl: './artist.component.html',
+  styleUrls: ['./artist.component.css', '../app.component.css']
+})
---
+++ assets/app/profile/profile.component.ts
@@ -0,0 +1,8 @@
+import {Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css', '../app.component.css']
+})
