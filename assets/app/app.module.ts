88011c1a6dc197cfa7b2814b31bf5928ba776f4a
Victor Rendon <victor.rs@eucledian.mx>
2017-11-27 23:22:50 -0600

    routes n module n shit

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule, Http } from "@angular/http";
 
 import { AppComponent } from "./app.component";
@@ -10,13 +10,21 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from './register/register.component';
 import { LoginService } from './providers/login-service';
 import { HomeComponent } from './home/home.component';
+import { DiscoverComponent } from './discover/discover.component';
+import { ArtistComponent } from './artist/artist.component';
+import { StudioComponent } from './studio/studio.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         LoginComponent,
         RegisterComponent,
-        HomeComponent
+        HomeComponent,
+        DiscoverComponent,
+        ArtistComponent,
+        StudioComponent,
+        ProfileComponent
     ],
     imports: [
       BrowserModule,
