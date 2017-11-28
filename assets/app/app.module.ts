69fae3ea6f80bc086f2daef3c76195550e6c52be
IvanAram <a01022584@itesm.mx>
2017-11-27 23:45:39 -0600

    login service

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { HttpModule, Http } from "@angular/http";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
 import { routing } from './app.routing';
