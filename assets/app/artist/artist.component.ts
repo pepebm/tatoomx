d19ffe4fc8f5576cb42deca4a6209bb6b02a6c7d
José Manuel <josemanuel@guruweb.mx>
2017-11-28 22:25:11 -0600

    Artists

--- assets/app/artist/artist.component.ts
+++ assets/app/artist/artist.component.ts
@@ -1,12 +1,23 @@
-import {Component, OnInit } from "@angular/core";
+import {Component, OnInit, Injectable } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { TattooistsService } from '../providers/tattooists-service';
 
 @Component({
   selector: 'artist',
   templateUrl: './artist.component.html',
   styleUrls: ['./artist.component.css', '../app.component.css']
 })
-
 export class ArtistComponent {
 
+  artists: any[];
+  constructor(private artistsService: TattooistsService){
+    this.artistsService.getAll().subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
