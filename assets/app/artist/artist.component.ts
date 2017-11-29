06ea036cc2e40282d62c9b1576716647f4751fbc
José Manuel <josemanuel@guruweb.mx>
2017-11-28 23:15:10 -0600

    http fix

--- assets/app/artist/artist.component.ts
+++ assets/app/artist/artist.component.ts
@@ -9,15 +9,35 @@ import { TattooistsService } from '../providers/tattooists-service';
 })
 export class ArtistComponent {
 
-  artists: any[];
+  artists: any;
+  independant: any = [];
+  studio: any = [];
   constructor(private artistsService: TattooistsService){
     this.artistsService.getAll().subscribe(
       data => {
-        console.log(data);
+        //console.log(data);
+        if(data.status >= 2){
+          console.log("Error");
+        }
+        else {
+          this.artists = data.data;
+
+          console.log(data.data);
+          console.log(this.artists);
+          for(let i = 0; i < this.artists.length; i++){
+            if(this.artists[i].studioId){
+              this.studio.push(this.artists[i]);
+            } else{
+              this.independant.push(this.artists[i]);
+            }
+          }
+        }
       },
       error => {
         console.log(error);
       }
     );
   }
+
+
 }
