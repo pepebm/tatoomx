909cffafb7282e8aa4618b5ef5e32c356a566adb
IvanAram <a01022584@itesm.mx>
2017-11-29 05:13:26 -0600

    asdasd!

--- assets/app/providers/studios-service.ts
+++ assets/app/providers/studios-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Http,Headers } from "@angular/http";
+import { SessionService } from './session-service';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -7,13 +8,13 @@ export class StudiosService {
 	private headers: any;
 	private serverURL: string = 'http://localhost:3000/';
 
-	constructor(private http: Http){
+	constructor(private http: Http, private sessionService: SessionService){
 		this.headers = new Headers();
 		this.headers.append('Content-Type','application/json');
 	}
 
 	getAll(){
-		return this.http.get(this.serverURL + 'studios/',{headers: this.headers}).map(res => res.json());
+		return this.http.get(this.serverURL + 'studios/' + this.sessionService.getSession().id,{headers: this.headers}).map(res => res.json());
 	}
 
 	getTattooists(studioId){
